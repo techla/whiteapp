3668a0cfd53134809e20057ece52db7e85bf6fef
Foad Ardalan <foad.ardalan@outlook.fr>
2021-07-26 04:17:45 +0200

    fix(all): test docker release

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 const {GIT_BRANCH: branch} = process.env;
 
 module.exports = {
-  branches: ["master", "release", {name: /^develop|(feature|bugfix)\/(BE|CC|SHOPPING)\.\d*\..*$/, prerelease: true}],
+  branches: ["master", "release", {name: "develop|(feature|bugfix)/(BE|CC|SHOPPING).d*.*", prerelease: true}],
   plugins: /^develop|(feature|bugfix)\/(BE|CC|SHOPPING)\.\d*\..*$/.test(branch) ? [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
