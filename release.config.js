88c49e3887510e28e04cbc22f01492bac854eb39
Foad Ardalan <foad.ardalan@outlook.fr>
2021-07-27 00:19:04 +0200

    feat(all): test docker release

--- release.config.js
+++ release.config.js
@@ -1,5 +1,29 @@
 const { GIT_BRANCH: branch } = process.env;
 const isDevelopmentBranch = /^develop|(feature|bugfix)\/(BE|CC|SHOPPING)\.\d*\..*$/.test(branch);
+const herokuPlugins = [
+  [
+    '@semantic-release/exec',
+    {
+      prepareCmd:
+        branch === 'develop'
+          ? 'yarn heroku:create techla-whiteapp || true'
+          : `yarn heroku:create techla-whiteapp-${branch.match(/\d+/g)[0]} || true`,
+    },
+  ],
+  '@semantic-release/commit-analyzer',
+  [
+    'semantic-release-heroku',
+    {
+      name: branch === 'develop' ? 'techla-whiteapp' : `techla-whiteapp-${branch.match(/\d+/g)[0]}`,
+    },
+  ],
+];
+const dockerPlugins = [
+  ['@semantic-release/exec', { prepareCmd: 'yarn docker:build ' }],
+  '@semantic-release/commit-analyzer',
+  ['semantic-release-docker', { name: 'techla/whiteapp' }],
+  '@semantic-release/changelog',
+];
 
 module.exports = {
   branches: [
@@ -9,32 +33,5 @@ module.exports = {
     { name: 'feature/*', prerelease: 'alpha' },
     { name: 'bugfix/*', prerelease: 'beta' },
   ],
-  plugins: isDevelopmentBranch
-    ? [
-        [
-          '@semantic-release/exec',
-          {
-            prepareCmd:
-              branch === 'develop'
-                ? 'yarn heroku:create techla-whiteapp || true'
-                : `yarn heroku:create techla-whiteapp-${branch.match(/\d+/g)[0]} || true`,
-          },
-        ],
-        '@semantic-release/commit-analyzer',
-        [
-          'semantic-release-heroku',
-          {
-            name:
-              branch === 'develop'
-                ? 'techla-whiteapp'
-                : `techla-whiteapp-${branch.match(/\d+/g)[0]}`,
-          },
-        ],
-      ]
-    : [
-        ['@semantic-release/exec', { prepareCmd: 'yarn docker:build ' }],
-        '@semantic-release/commit-analyzer',
-        ['semantic-release-docker', { name: 'techla/whiteapp' }],
-        '@semantic-release/changelog',
-      ],
+  plugins: isDevelopmentBranch ? herokuPlugins : dockerPlugins,
 };
